67c5c9f5eec21bc7d54a2cd8edb6fddeb54144a5
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-09-12 12:43:41 -0700

    added second route and template, added header partial and included into templates

--- express_server.js
+++ express_server.js
@@ -13,9 +13,15 @@ app.get('/', (req, res) => {
   res.end('Hello!');
 });
 
+//when /urls request, render views/urls_index.ejs and pass in templateVars
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render('urls_show', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
