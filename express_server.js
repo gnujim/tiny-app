84bd525d21b34820db27ddb6b0e20c1386a0e560
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-09-12 14:21:12 -0700

    added function to generate 6 random alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.set('view engine', 'ejs');
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+//generates 6 random alphanumeric characters 'unique shortURL'
+//for now, global scope
+const generateRandomString = () => {
+  var shortURL = Math.random()
+    .toString(36)
+    .substr(2, 6);
+  return shortURL;
+};
+
+const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
@@ -22,13 +30,13 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
-app.post('urls', (req, res) => {
+app.post('/urls', (req, res) => {
   console.log(req.body);
   res.send('Ok');
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls:id', (req, res) => {
