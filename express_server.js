cf446d4427634aea04f3c93d42abb1503a266362
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-09-15 13:55:06 -0700

    refactored to have res.locals, fixed bcrypt error, updated urlsForUser function

--- express_server.js
+++ express_server.js
@@ -5,8 +5,9 @@ const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-//generates 6 random alphanumeric characters 'unique shortURL'
-//for now, global scope
+// ** FUNCTIONS **
+
+// generates 6 random alphanumeric characters 'unique shortURL'
 function generateRandomString() {
   var charNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   charNum += charNum.toLowerCase() + '0123456789';
@@ -21,14 +22,25 @@ function generateRandomString() {
   return shortURL;
 }
 
-//middleware
+// function that returns array of user specific urlPair objects
+function urlsForUser(id) {
+  let urls = [];
+  for (key in urlDatabase) {
+    if (id === urlDatabase[key].userID) {
+      let urlPairs = {
+        shortUrl: key,
+        longUrl: urlDatabase[key].longURL
+      };
+      urls.push(urlPairs);
+    }
+  }
+  return urls;
+}
+
+// ** MIDDLEWARE **
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//res.locals
-// app.use((req, res, next) => {
-
-// });
-
+// cookie-session middleware
 app.use(
   cookieSession({
     name: 'session',
@@ -36,10 +48,20 @@ app.use(
   })
 );
 
-//use EJS for template
+// res.locals
+app.use(function locals(req, res, next) {
+  res.locals.user = users[req.session.user_id];
+  res.locals.shortURL = req.params.id;
+  res.locals.longURL = urlDatabase[req.params.id];
+  next();
+});
+
+// use EJS for template
 app.set('view engine', 'ejs');
 
-//(not good practice to have in global scope)
+// ** HARDCODED DATABASE OBJECTS **
+
+// (not good practice to have in global scope)
 var urlDatabase = {
   b2xVn2: {
     userID: 'userRandomID',
@@ -55,79 +77,70 @@ var urlDatabase = {
   }
 };
 
-//global users object
+// global users object
 var users = {
   userRandomID: {
     id: 'userRandomID',
     email: 'user@example.com',
     password: '$2a$10$ecEcOT15ekLpW5iMg8dALu7HRzJL5Cr6qx1FZUmFPOk5oKHLmxb9S'
-    //'purple-monkey-dinosaur'
+    // 'purple-monkey-dinosaur'
   },
   user2RandomID: {
     id: 'user2RandomID',
     email: 'user2@example.com',
     password: '$2a$10$hQOInZ/Kl.FyH13eU343UezvdzGWd9zU3ryDw3d9trgOCvUMhmmyi'
-    //'dishwasher-funk'
+    // 'dishwasher-funk'
   }
 };
 
 // ** ROUTES **
 
-//root
+// if user is logged in, head to /urls
+// if not logged in, prompt to /login
 app.get('/', (req, res) => {
-  res.end('Hello!');
+  if (req.session.user_id) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
 });
 
-//when /urls request, render views/urls_index.ejs and pass in templateVars
+// when /urls request, render views/urls_index.ejs and pass in templateVars
 app.get('/urls', (req, res) => {
   let userUrls = urlsForUser(req.session.user_id);
-  console.log(userUrls);
-  console.log(req.session.user_id);
   let templateVars = {
-    user: users[req.session.user_id],
     urls: userUrls
   };
   res.render('urls_index', templateVars);
 });
 
-//render urls_new page ***
+// render urls_new page ***
 app.get('/urls/new', (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  //if logged in
-  for (id in users) {
-    if (req.session.user_id) {
-      res.render('urls_new', templateVars);
-    } else {
-      res.render('login', templateVars);
-    }
+  if (req.session.user_id) {
+    res.render('urls_new');
+  } else {
+    res.render('login');
   }
 });
 
-//once redirected, render urls_show page
+// once redirected, render urls_show page
 app.get('/urls/:id', (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id],
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
-  };
-  console.log(req.session.user_id);
   if (req.session.user_id === urlDatabase[req.params.id].userID) {
-    res.render('urls_show', templateVars);
+    res.render('urls_show');
   } else {
-    //display message or prompt
+    // display message or prompt
     res.status(403).send('NOOOO');
-  } //else if?? url with matching id does not belong to them (if logged in)
+  } // else if?? url with matching id does not belong to them (if logged in)
 });
 
-//redirect short urls
+// redirect short urls
 app.get('/u/:shortURL', (req, res) => {
   let longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
-//obtain longURL from user, create shortURL, add to database
+// obtain longURL from user, create shortURL, add to database
 app.post('/urls', (req, res) => {
-  console.log(req.body);
   let long = req.body.longURL;
   let short = generateRandomString();
   let id = req.session.user_id;
@@ -139,83 +152,84 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${short}`);
 });
 
-//updates a URL resource and redirects to urls_index
+// updates a URL resource and redirects to urls_index
 app.post('/urls/:id', (req, res) => {
-  //modifies long url w corresponding short url
+  // modifies long url w corresponding short url
   res.redirect('/urls');
 });
 
-//deletes id and value from urlDatabase
+// deletes id and value from urlDatabase
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
-//GET login
+// GET login
 app.get('/login', (req, res) => {
   res.render('login');
 });
 
-//GET register returns a page that includes a form w email and password
+// GET register returns a page that includes a form w email and password
 app.get('/register', (req, res) => {
   res.render('register');
 });
 
-//login handler
+// login handler
 app.post('/login', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  let user;
-  for (key in users) {
-    console.log(key);
+  let user_id;
+  for (let key in users) {
     if (email === users[key].email) {
-      user = key;
+      user_id = key;
     }
   }
-  if (user) {
-    if (bcrypt.compareSync(password, users[user].password)) {
-      req.session.user_id = 'user_id';
+  if (user_id) {
+    if (bcrypt.compareSync(password, users[user_id].password)) {
+      req.session.user_id = user_id;
     } else {
       res.status(403).send('Forbidden. Invalid password.');
     }
   } else {
     res.status(403).send('Forbidden. User does not exist.');
   }
-  res.redirect('/');
+  res.redirect('/urls');
 });
 
-//register handler
+// register handler
 app.post('/register', (req, res) => {
   let userEmail = req.body.email;
-  //let userPassword = req.body.password;
-  let hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  let userId = generateRandomString();
+  let userPassword = req.body.password;
+  if (!userEmail || !userPassword) {
+    res.status(400).send('Bad Request');
+  }
   for (key in users) {
     if (userEmail === users[key].email) {
       res.status(400).send('Bad Request. Email already exists.');
     }
   }
-  if (!userEmail || !hashedPassword) {
-    res.status(400).send('Bad Request');
-  }
+  let userId = generateRandomString();
+  let hashedPassword = bcrypt.hashSync(userPassword, 10);
   users[userId] = {
     id: userId,
     email: userEmail,
     password: hashedPassword
   };
-  console.log(users[userId] + users[userId]);
-  req.session.user_id = 'user_id';
+  req.session.user_id = userId;
   res.redirect('/urls');
 });
 
-//POST logout
+// POST logout
 app.post('/logout', (req, res) => {
-  //req.session = null;
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('/urls');
 });
 
 //
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 //--------------------------- other?
 
 //
@@ -228,29 +242,7 @@ app.get('/hello', (req, res) => {
   res.end('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-//?????? from lecture
-//create own middleware
 app.use((req, res, next) => {
   req.user = users.find(user => user.id === req.cookies.userId);
   next();
-});
-
-//res.locals.user
-
-//sessions on the REQuest
-
-//function that returns array of urls that belong to user
-function urlsForUser(id) {
-  let urls = [];
-  for (key in urlDatabase) {
-    if (id === urlDatabase[key].userID) {
-      urls.push(key);
-    }
-  }
-  return urls;
-}
-
-//
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
